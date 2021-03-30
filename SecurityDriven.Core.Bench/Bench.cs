a6b80795cb24c050b977475e5cb0517758b54ae2
Stan Drapkin <sdrapkin@sdprime.com>
2021-03-30 17:29:37 -0400

    Revamp Bench with action Runner

--- SecurityDriven.Core.Bench/Bench.cs
+++ SecurityDriven.Core.Bench/Bench.cs
@@ -10,7 +10,7 @@ namespace SecurityDriven.Core.Bench
 		static void Main(string[] args)
 		{
 			var sw = new Stopwatch();
-			const long ITER = 5_000_000L * 1L;
+			const long ITER = 5_000_000L * 2L;
 
 			$"{nameof(Environment.ProcessorCount)}: {Environment.ProcessorCount}".Dump();
 			$"{nameof(CryptoRandom.BYTE_CACHE_SIZE)}: {CryptoRandom.BYTE_CACHE_SIZE}".Dump();
@@ -29,62 +29,77 @@ namespace SecurityDriven.Core.Bench
 			//return;
 			const int REPS = 6;
 
+			const bool IS_SEQUENTIAL = false;
+			const bool IS_PARALLEL = true;
+
 			for (int j = 0; j < REPS; ++j)
 			{
 				{
 					sw.Restart();
-					for (var k = 0L; k < ITER; ++k)
-					//Parallel.For(0L, ITER, i =>
+					Runner(ITER, IS_SEQUENTIAL, IS_PARALLEL, i =>
 					{
 						var data = default(TestStruct);
 						var span = Utils.AsSpan(ref data);
 						cr.NextBytes(span);
-					}
+					});
 					sw.Stop();
 					$"{sw.Elapsed} {nameof(Utils.AsSpan) + " " + nameof(cr.NextBytes)} {cr.GetType().Name}".Dump();
 				}
 
 				{
 					sw.Restart();
-					for (var k = 0L; k < ITER; ++k)
-					//Parallel.For(0L, ITER, i =>
+					Runner(ITER, IS_SEQUENTIAL, IS_PARALLEL, i =>
 					{
 						var data = default(TestStruct);
 						cr.FillStruct(ref data);
-					}
+					});
 					sw.Stop();
 					$"{sw.Elapsed} {nameof(cr.FillStruct)} {cr.GetType().Name}".Dump();
 				}
 
 				{
 					sw.Restart();
-					for (var k = 0L; k < ITER; ++k)
-					//Parallel.For(0L, ITER, i =>
+					Runner(ITER, IS_SEQUENTIAL, IS_PARALLEL, i =>
 					{
 						var data = default(TestStruct);
 						CryptoRandom.Instance.FillStruct(ref data);
-					}
+					});
 					sw.Stop();
 					$"{sw.Elapsed} {nameof(CryptoRandom)}.{nameof(CryptoRandom.Instance)}.{nameof(CryptoRandom.FillStruct)} {cr.GetType().Name}".Dump();
 				}
 
 				{
 					sw.Restart();
-					for (var k = 0L; k < ITER; ++k)
-					//Parallel.For(0L, ITER, i =>
+					Runner(ITER, IS_SEQUENTIAL, IS_PARALLEL, i =>
 					{
 						Guid.NewGuid();
-					}
+					});
 					sw.Stop();
-					$"{sw.Elapsed} {nameof(Guid.NewGuid)}".Dump();
+					$"{sw.Elapsed} {typeof(Guid).FullName}.{nameof(Guid.NewGuid)}".Dump();
+				}
+
+				{
+					sw.Restart();
+					Runner(ITER, IS_SEQUENTIAL, IS_PARALLEL, i =>
+					{
+						cr.NewSqlServerGuid();
+					});
+					sw.Stop();
+					$"{sw.Elapsed} {cr.GetType().FullName}.{nameof(cr.NewSqlServerGuid)}".Dump();
 				}
 				"".Dump();
-			}
-		}
+			}// REPS
+		}//Main()
 
 		[StructLayout(LayoutKind.Sequential, Size = 16)]
 		public struct TestStruct { }
 
+		static void Runner(long iterations, bool isSequential, bool isParallel, Action<long> action)
+		{
+			if (isSequential) for (var i = 0L; i < iterations; ++i) action(i);
+
+			if (isParallel) Parallel.For(0L, iterations, i => action(i));
+		}//Runner()
 	}//class Bench
 
 	public static class ObjExtensions
