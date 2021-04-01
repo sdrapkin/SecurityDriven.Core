437520573338f256ee16cb7b1646062b8e0cb09f
Stan Drapkin <sdrapkin@sdprime.com>
2021-04-01 14:07:29 -0400

    Update version-check

--- SecurityDriven.Core.Tests/SanityTests.cs
+++ SecurityDriven.Core.Tests/SanityTests.cs
@@ -9,15 +9,19 @@ namespace SecurityDriven.Core.Tests
 	public class SanityTests
 	{
 		[DataTestMethod]
-		[DataRow(1, 0, 0, 0)]
-		public void VersionCheck(int major, int minor, int build, int revision)
+		[DataRow(1, 0, 0, "alpha")]
+		public void VersionCheck(int major, int minor, int build, string releaseType = "")
 		{
 			Assembly assembly = typeof(CryptoRandom).Assembly;
 			FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
-			var expectedVersion = new Version(major, minor, build, revision);
+			var expectedFileVersion = new Version(major, minor, build, 0);
 
-			Assert.IsTrue(new Version(fvi.ProductVersion) == expectedVersion);
-			Assert.IsTrue(new Version(fvi.FileVersion) == expectedVersion);
+			Assert.IsTrue(new Version(fvi.FileVersion) == expectedFileVersion);
+			var expectedProductVersion = $"{major}.{minor}.{build}";
+
+			if (!string.IsNullOrEmpty(releaseType))
+				expectedProductVersion += $"-{releaseType}";
+			Assert.IsTrue(fvi.ProductVersion == expectedProductVersion);
 
 			assembly.GetModules()[0].GetPEKind(out var kind, out var machine);
 			Assert.IsTrue(kind == PortableExecutableKinds.ILOnly);
@@ -29,7 +33,7 @@ namespace SecurityDriven.Core.Tests
 				"[NETCOREAPP] " 
 #endif
 				+ $"[{Environment.Version}] [{System.Runtime.InteropServices.RuntimeEnvironment.GetRuntimeDirectory()}]";
-			Console.WriteLine($"[{assembly.FullName}] version: [{expectedVersion}]");
+			Console.WriteLine($"[{assembly.FullName}] file-version: [{expectedFileVersion}] product-version: [{expectedProductVersion}]");
 			Console.WriteLine(environment);
 		}//VersionCheck()
 	}//class SanityTests
