a0baa0d3bee0751519e92f5ebce141529cde83b6
Stan Drapkin <sdrapkin@sdprime.com>
2021-08-30 11:34:05 -0400

    Perf improvements to FastGuid

--- SecurityDriven.Core/FastGuid.cs
+++ SecurityDriven.Core/FastGuid.cs
@@ -8,17 +8,14 @@ namespace SecurityDriven.Core
 	internal static class FastGuid
 	{
 		const int GUIDS_PER_THREAD = 512; //keep it power-of-2
+		const int GUID_SIZE_IN_BYTES = 16;
 		[ThreadStatic] static Container ts_data;
-
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		static Container CreateContainer() => ts_data = new();
 
 		static Container LocalContainer
 		{
 			[MethodImpl(MethodImplOptions.AggressiveInlining)]
-			get => ts_data ?? CreateContainer();
+			get => ts_data ??= new();
 		}
-
 		sealed class Container
 		{
 			Guid[] _guids = GC.AllocateUninitializedArray<Guid>(GUIDS_PER_THREAD);
@@ -27,12 +24,16 @@ namespace SecurityDriven.Core
 			[MethodImpl(MethodImplOptions.AggressiveInlining)]
 			public Guid NextGuid()
 			{
-				var guids = _guids;
+				ref var guid0 = ref MemoryMarshal.GetArrayDataReference(_guids);
 				int idx = _idx++ & (GUIDS_PER_THREAD - 1);
-				if (idx == 0) RandomNumberGenerator.Fill(MemoryMarshal.Cast<Guid, byte>(guids));
+				if (idx == 0)
+				{
+					RandomNumberGenerator.Fill(
+						MemoryMarshal.CreateSpan<byte>(ref Unsafe.As<Guid, byte>(ref guid0), GUIDS_PER_THREAD * GUID_SIZE_IN_BYTES));
+				}
 
-				var guid = guids[idx];
-				guids[idx] = default; // prevents Guid leakage
+				var guid = Unsafe.Add(ref guid0, idx);
+				Unsafe.Add(ref guid0, idx) = default; // prevents Guid leakage
 				return guid;
 			}//NextGuid()
 		}//class Container
