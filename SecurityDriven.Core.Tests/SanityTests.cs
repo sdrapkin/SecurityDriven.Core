2151a7e46a93d0d4e3507bfae2c017e14e79e962
Stan Drapkin <sdrapkin@sdprime.com>
2023-05-18 13:39:34 -0400

    Fixing Sanity Tests

--- SecurityDriven.Core.Tests/SanityTests.cs
+++ SecurityDriven.Core.Tests/SanityTests.cs
@@ -16,6 +16,7 @@ namespace SecurityDriven.Core.Tests
 			FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
 			var expectedFileVersion = new Version(major, minor, build, 0);
 
+			Console.WriteLine(new Version(fvi.FileVersion) + " " + fvi.FileVersion);
 			Assert.IsTrue(new Version(fvi.FileVersion) == expectedFileVersion);
 			var expectedProductVersion = $"{major}.{minor}.{build}";
 
