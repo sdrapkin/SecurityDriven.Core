843f543870a137f9ed7e0aca665ced33ee1dd942
Stan Drapkin <sdrapkin@sdprime.com>
2023-10-05 15:33:33 -0400

    FastGuid update. Test.Sdk version update.

--- SecurityDriven.Core/FastGuid.cs
+++ SecurityDriven.Core/FastGuid.cs
@@ -6,40 +6,46 @@ using System.Security.Cryptography;
 namespace SecurityDriven.Core
 {
 	/// <summary>Represents a globally unique identifier (GUID).</summary>
-	internal static class FastGuid
+	public static class FastGuid
 	{
-		// Copyright (c) 2022 Stan Drapkin
+		// Copyright (c) 2023 Stan Drapkin
 		// LICENSE: https://github.com/sdrapkin/SecurityDriven.FastGuid
 
-		const int GUIDS_PER_THREAD = 256; //keep it power-of-2
+		const int GUIDS_PER_THREAD = 1 << 8; // 256 (keep it power-of-2)
 		const int GUID_SIZE_IN_BYTES = 16;
+
+		[StructLayout(LayoutKind.Sequential, Size = GUIDS_PER_THREAD * GUID_SIZE_IN_BYTES, Pack = 1)]
+		struct Guids
+		{
+			Guid guid0;
+
+			public Span<Guid> AsSpanGuid() => MemoryMarshal.CreateSpan(ref guid0, GUIDS_PER_THREAD);
+		}//Guids
 
 		struct Container
 		{
-			public Guid[] _guids;
-			public int _idx;
-		}
+			public Guids _guids; // do not move, should be 1st
+			public byte _idx; // wraps around on 256 (GUIDS_PER_THREAD)
+		}//Container
 
-		[ThreadStatic] static Container ts_data;
+		[ThreadStatic] static Container ts_container; //ts stands for "ThreadStatic"
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="Guid"/> structure.
-		/// </summary>
+		/// <summary>Initializes a new instance of the <see cref="Guid"/> structure.</summary>
 		/// <returns>A new <see cref="Guid"/> struct.</returns>
 		/// <remarks>Faster alternative to <see cref="Guid.NewGuid"/>.</remarks>
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static Guid NewGuid()
 		{
-			ref var guid0 = ref MemoryMarshal.GetArrayDataReference(ts_data._guids ??= GC.AllocateUninitializedArray<Guid>(GUIDS_PER_THREAD));
-			int idx = ts_data._idx++ & (GUIDS_PER_THREAD - 1);
+			ref Container container = ref ts_container;
+			byte idx = container._idx++;
 			if (idx == 0)
 			{
 				RandomNumberGenerator.Fill(
-					MemoryMarshal.CreateSpan<byte>(ref Unsafe.As<Guid, byte>(ref guid0), GUIDS_PER_THREAD * GUID_SIZE_IN_BYTES));
+					MemoryMarshal.CreateSpan<byte>(ref Unsafe.As<Container, byte>(ref container), GUIDS_PER_THREAD * GUID_SIZE_IN_BYTES));
 			}
-
-			var guid = Unsafe.Add(ref guid0, idx);
-			Unsafe.Add(ref guid0, idx) = default; // prevents Guid leakage
+			Span<Guid> span = container._guids.AsSpanGuid();
+			Guid guid = span[idx];
+			span[idx] = default;
 			return guid;
 		}//NewGuid()
 	}//class FastGuid
